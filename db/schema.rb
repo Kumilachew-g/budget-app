696c8b23821ec29133b4713ae9931032f79ded95
Kumilachew-g <kumilachew1212@gmail.com>
2022-12-19 23:05:44 +0300

    Update database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_19_194505) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_19_200139) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_19_194505) do
     t.float "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_entities_on_user_id"
+  end
+
+  create_table "entity_groups", force: :cascade do |t|
+    t.bigint "entity_id", null: false
+    t.bigint "group_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["entity_id"], name: "index_entity_groups_on_entity_id"
+    t.index ["group_id"], name: "index_entity_groups_on_group_id"
   end
 
   create_table "groups", force: :cascade do |t|
@@ -26,6 +37,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_19_194505) do
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +47,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_19_194505) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "entities", "users"
+  add_foreign_key "entity_groups", "entities"
+  add_foreign_key "entity_groups", "groups"
+  add_foreign_key "groups", "users"
 end
