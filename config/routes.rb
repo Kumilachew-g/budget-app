76f4ac850d9aedef0697ebf3ad23914f4aee4fa1
Kumilachew-g <kumilachew1212@gmail.com>
2022-12-20 17:00:50 +0300

    Switch default form to new category page

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     authenticated :user do
-      root 'groups#new', as: :authenticated_root
+      root 'groups#index', as: :authenticated_root
     end
 
     unauthenticated do
