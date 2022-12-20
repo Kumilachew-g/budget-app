894dd5d1a12b5ded68244dba7b6ce09fb0d5860f
Kumilachew-g <kumilachew1212@gmail.com>
2022-12-20 12:09:25 +0300

    Add devise schema to user

--- db/migrate/20221220084011_devise_create_users.rb
+++ db/migrate/20221220084011_devise_create_users.rb
@@ -2,7 +2,7 @@
 
 class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
-    create_table :users do |t|
+    change_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -31,9 +31,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-
-
-      t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
