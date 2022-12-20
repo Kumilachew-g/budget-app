2059d69e2a5c09ac42cf937d7c6c1454faa28ca2
Kumilachew-g <kumilachew1212@gmail.com>
2022-12-20 17:29:17 +0300

    Fix categories error when add transaction

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,5 +1,5 @@
 class EntitiesController < ApplicationController
-  before_action :set_entity, only: %i[ show edit update destroy ]
+  before_action :set_entity, only: %i[show edit update destroy]
 
   # GET /entities or /entities.json
   def index
@@ -7,8 +7,7 @@ class EntitiesController < ApplicationController
   end
 
   # GET /entities/1 or /entities/1.json
-  def show
-  end
+  def show; end
 
   # GET /entities/new
   def new
@@ -17,17 +16,22 @@ class EntitiesController < ApplicationController
   end
 
   # GET /entities/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /entities or /entities.json
   def create
-    @entity = Entity.new(entity_params)
+    params = entity_params
+    @entity = Entity.new(name: params[:name], amount: params[:amount])
     @entity.user_id = current_user.id
+    @categories_ids = params[:group_ids]
+    @categories_ids.each do |id|
+      group = Group.find(id) unless id == ''
+      @entity.groups.push(group) unless group.nil?
+    end
 
     respond_to do |format|
       if @entity.save
-        format.html { redirect_to groups_url, notice: "Transaction was successfully created." }
+        format.html { redirect_to groups_url, notice: 'Transaction was successfully created.' }
         format.json { render :show, status: :created, location: @entity }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -40,7 +44,7 @@ class EntitiesController < ApplicationController
   def update
     respond_to do |format|
       if @entity.update(entity_params)
-        format.html { redirect_to entity_url(@entity), notice: "Transaction was successfully updated." }
+        format.html { redirect_to entity_url(@entity), notice: 'Transaction was successfully updated.' }
         format.json { render :show, status: :ok, location: @entity }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -54,19 +58,20 @@ class EntitiesController < ApplicationController
     @entity.destroy
 
     respond_to do |format|
-      format.html { redirect_to entities_url, notice: "Transaction was successfully destroyed." }
+      format.html { redirect_to groups_url, notice: 'Transaction was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_entity
-      @entity = Entity.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def entity_params
-      params.fetch(:entity, {})
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_entity
+    @entity = Entity.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def entity_params
+    params.require(:entity).permit(:name, :amount, group_ids: [])
+  end
 end
