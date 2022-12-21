6fc0e0c156a3672a98a9362559c54b23a95549a0
Kumilachew-g <kumilachew1212@gmail.com>
2022-12-21 12:02:26 +0300

    Update amount validation

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -3,5 +3,5 @@ class Entity < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true, length: { maximum: 250 }
-  validates :amount, numericality: { greater_than_or_equal_to: 0 }
+  validates :amount, numericality: { greater_than: 0 }
 end
